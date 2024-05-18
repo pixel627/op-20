49057f8debc94de94a33234d30f9a3f512de19ff
pixel627 <pixelguy627@gmail.om>
2024-05-18 11:26:59 +0300

    medianFilter(3)

--- libs/thread_io/thread_io.h
+++ libs/thread_io/thread_io.h
@@ -8,6 +8,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int compareInts1(const void *intPtr1, const void *intPtr2);
 void printMatrix(int *matrix, int n, int m);
 
 
@@ -16,6 +17,7 @@ void fill_matrix(int *matrix, int sizeOfMatrix, int *query, int queryCount);
 int countNeighbors(int *matrix, size_t n, size_t m, size_t col, size_t row);
 void liveGame(int *matrix, size_t n, size_t m);
 
-void MedianFilter(int *matrix, int filter);
+int calculateMedianVector(int *matrix, int max_col, int row, int col, int *arrayOfNums, int filter_size);
+void medianFilter(int *matrix, int filter, int n, int m);
 
 #endif //OP_20_THREAD_IO_H
